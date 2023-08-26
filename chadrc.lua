486762e094e9dc1c7365c31d30b7a01e24d807cf
Gleb Fedotov <innercoding@gmail.com>
2023-08-26 21:44:52 +0300

    add nvdash image

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,35 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {
+  theme = 'catppuccin',
+  theme_toggle = { "catppuccin", "one_light" },
+  hl_override = {
+    NvDashAscii = {
+      fg = "blue",
+      bg = "black",
+    },
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = {
+"              ▒██████████▒              ",
+"              ███▓▓██▓▓██▓              ",
+"              ███▓▓▓▓████▓              ",
+"              ███▓▓██▓▓██▓              ",
+"   ░█████████░▒██████████▒░█████████░   ",
+"   ██▓▓██▓▓██░▒▓▓▓▓▓▓▓▓▓▓▒░██▓▓██████   ",
+" █▒██▓▓▓▓▓▓██░▒▒▒▒▒▒▒▒▒▒▒▒░██▓▓██████▒█ ",
+"█ ▒██▓▓██▓▓██░▓██████████▓░██▓▓▓▓▓▓██▒ █",
+"█ ▒██████████░███████▓▓███░██████████░ █",
+"█ ░▓▓▓▓▓▓▓▓▓▓░███▓▓██▓▓███░▓▓▓▓▓▓▓▓▓▓░ █",
+" █           ░███▓▓▓▓▓▓███░           █ ",
+"  ███████████ ███████████▓ ▓██████████  ",
+"            █ ▒▓▓▓▓▓▓▓▓▓▓▒ █            ",
+"             █░          ░█             ",
+"              ████████████              ",
+    }
+  },
+}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
